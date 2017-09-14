6f2a4ad600b27291ec7c83f59e4995780f914a28
Riki Montgomery and Sara Spink <riki@>
2017-09-14 11:55:37 -0700

    create initial views

--- app.rb
+++ app.rb
@@ -3,3 +3,16 @@ require ('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require ('./lib/contacts')
 require ('pry')
+
+get('/') do
+
+  erb(:input)
+end
+
+post('/') do
+  @first_name = params.fetch["first_name"]
+  @last_name = params.fetch["last_name"]
+  contact_info = Contact.new({:first_name=> @first_name, :last_name=> @last_name})
+
+  erb(:output)
+end
